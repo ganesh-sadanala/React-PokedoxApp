a50914384140eb96e5338547f338634d76814df0
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-07-02 12:45:39 +0530

    Design the Pokedex component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./App.css";
+import { pokemonData } from "./../data/pokemonData";
+import { Pokedex } from "./pokedex/Pokedex";
+
 class App extends React.Component {
   constructor() {
     super();
-    state = {};
+    this.state = {
+      searchField: "",
+      pokemons: [],
+      selectedPokemon: null,
+    };
+  }
+  componentWillMount() {}
+  componentDidMount() {
+    this.setState({ pokemons: pokemonData });
   }
   render() {
     return (
       <div className="App">
         <h1>Pokedex</h1>
+        <Pokedex />
       </div>
     );
   }
