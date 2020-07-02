6454acca985e6779fc1bb2327a84e140040c2189
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-07-02 11:56:13 +0530

    Project strucutre is ready

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  constructor() {
+    super();
+    state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Pokedex</h1>
+      </div>
+    );
+  }
+}
+export default App;
